46803c8b16fe2279ef4e51540911a6f92c87f45f
AnawaratneMANA <it19086408@my.sliit.lk>
2021-08-15 23:25:39 +0530

    Custom Table Created final.

---
+++ src/Component/Tutor/CourseMgntInt/CustomButtons/CustomButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            <p>Login Component</p>
+        </div>
+    )
+}
+
+export default Login
--- src/Component/Tutor/CourseMgntInt/CustomRow/CustomRow.jsx
+++ src/Component/Tutor/CourseMgntInt/CustomRow/CustomRow.jsx
@@ -5,11 +5,23 @@ function CustomRow() {
     return (
         <div className="custom-row-component">
             <div className="custom-row-component-first-column">
-                <div className="custom-row-component-header">English Literature</div>
-                <div className="custom-row-component-description">Course Body</div>
+                <div className="custom-row-component-header">
+                    English Literature
+                </div>
+                <div className="custom-row-component-description">
+                    Literature broadly is any collection
+                    of written work,
+                    but it is also used more narrowly
+                    for writings specifically
+                    considered to be an art form
+                </div>
             </div>
             <div className="custom-row-component-second-column">
-                <div className="custom-row-component-second-description">About the course.</div>
+                <div className="custom-row-component-second-description">
+                    dogs that like to sleep all day
+                    dogs that just don't mind foxes jumping over them
+                    dogs that have worked hard all and are now resting.
+                </div>
             </div>
             <div className="custom-row-component-three-column">
                 {/*Custom Button Stack*/}
@@ -18,7 +30,6 @@ function CustomRow() {
                     <button>Custom Button</button>
                     <button>Custom Button</button>
                 </div>
-
             </div>
         </div>
     )
