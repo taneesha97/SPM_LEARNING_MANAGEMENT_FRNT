278a6a2ceaa2ef0786c24198e82489a5fddd081b
AnawaratneMANA <it19086408@my.sliit.lk>
2021-08-17 00:12:50 +0530

    form updated for the course content.

--- src/Component/Tutor/CourseMgntInt/CourseMgntInt.jsx
+++ src/Component/Tutor/CourseMgntInt/CourseMgntInt.jsx
@@ -1,70 +1,39 @@
 import React from 'react'
 import "./CourseMgntInt.css"
 import "../../../../node_modules/bootstrap/dist/css/bootstrap.css"
-import Tile1 from "../Header/TileComponent/Tile1";
 import CustomRow from "./CustomRow/CustomRow";
+
 function CourseMgntInt() {
     return (
         <React.Fragment>
             <div className="course-component">
                 <div className="input-form-container">
-                <form className="input-form">
-                        <div className="course-component-header">
-                            <div className="section-header">Course Form</div>
-                        </div>
+                        <form className="form">
+                            <div className="course-component-header">
+                                <div className="section-header">Course Form</div>
+                                <div className="second-header">Course Form</div>
+                            </div>
 
-                        <div className="row container-row row-cols-auto">
-                            <div className="col">
-                                <span>Course Name: </span>
-                            </div>
-                        </div>
-                        <div className="row container-row row-cols-auto">
-                            <div className="col">
-                                <input type="text" className="input-form-field"/>
-                            </div>
-                        </div>
+                            <label htmlFor="fname">Name</label>
+                            <input type="text" id="fname" name="firstname" placeholder="Your name.." className="form-input"/>
 
-                        <div className="row container-row row-cols-auto">
-                            <div className="col">
-                                <span>Number of Chapters: </span>
-                            </div>
-                        </div>
-                        <div className="row container-row row-cols-auto">
-                            <div className="col">
-                                <input type="text" className="input-form-field"/>
-                            </div>
-                        </div>
+                            <label htmlFor="lname">Number of Chapters</label>
+                            <input type="text" id="lname" name="lastname" placeholder="Number of Chapters.." className="form-input"/>
 
-                        <div className="row container-row row-cols-auto">
-                            <div className="col">
-                                <span>Price: </span>
-                            </div>
-                        </div>
-                        <div className="row container-row row-cols-auto">
-                            <div className="col">
-                                <input type="text" className="input-form-field"/>
-                            </div>
-                        </div>
+                            <label htmlFor="lname">Price</label>
+                            <input type="text" id="lname" name="lastname" placeholder="Price.." className="form-input"/>
 
-                        <div className="row container-row row-cols-auto">
-                            <div className="col">
-                                <span>Description: </span>
-                            </div>
-                        </div>
-                        <div className="row container-row row-cols-auto">
-                            <div className="col">
-                                <input type="text" className="input-form-field"/>
-                            </div>
-                        </div>
+                            <label htmlFor="lname">Description</label>
+                            <input type="text" id="lname" name="lastname" placeholder="Description.." className="form-input"/>
 
-                        <div className="course-button-group button-row">
-                            <button className="dark-button">Add</button>
-                            <button className="light-button">Reset</button>
-                        </div>
-                    </form>
+                            <div className="course-button-group button-row">
+                                <button className="dark-button">Add</button>
+                                <button className="light-button">Reset</button>
+                            </div>
+                        </form>
                 </div>
                 <div className="course-table">
-                    <div className="section-header dark-header"> Active Courses </div>
+                    <div className="section-header dark-header"> Active Courses</div>
                     <div>
                         {/*Insert a custom video*/}
                         <CustomRow/>
@@ -76,4 +45,5 @@ function CourseMgntInt() {
         </React.Fragment>
     )
 }
+
 export default CourseMgntInt
