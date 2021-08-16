d9a0cfa2b54a89227310b077c86eb14a3e295b3f
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-08-16 17:06:17 +0530

    login interface sketching 30% over
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import './Login.css'
+import LoginLogo from './Images/LoginLogo.png';
 
 function Login() {
     return (
         <div>
             <p>Login Component</p>
             <div className="login">
+                <div>
+                    <img src= {LoginLogo}  className="login-image1"/>
+                </div>
+                <div className="login-info2">
+                    <h2 className="login-info2-main">Welcome</h2>
+                    <h4 className="login-info2-second">User1</h4>
+                </div>
+                <div className="login-info">
+                    <div className="login-info3">
+                        <h2 className="login-info3-main">Welcome</h2>
+                        <h4 className="login-info3-second">User1</h4>
+                    </div>
+                </div>
             </div>
 
-
-            {/*<div className="login">*/}
-            {/*    <div className="login-left">*/}
-            {/*        <h1>hi</h1>*/}
-            {/*    </div>*/}
-            {/*    <div className="login-info">*/}
-            {/*        <h2 className="login-main">hwwww</h2>*/}
-            {/*        <p className="login-second">pppppppp</p>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </div>
     )
 }
