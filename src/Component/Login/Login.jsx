1712f40efde20536e1aa759042b7e8ab26395231
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-08-16 12:51:40 +0530

    login interface sketching over
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import './Login.css'
 
 function Login() {
     return (
         <div>
             <p>Login Component</p>
+            <div className="login">
+            </div>
 
+
+            {/*<div className="login">*/}
+            {/*    <div className="login-left">*/}
+            {/*        <h1>hi</h1>*/}
+            {/*    </div>*/}
+            {/*    <div className="login-info">*/}
+            {/*        <h2 className="login-main">hwwww</h2>*/}
+            {/*        <p className="login-second">pppppppp</p>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
     )
 }
