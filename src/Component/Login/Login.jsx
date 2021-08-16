afd383eb660b36ba983bfa859b90f9480e47d1fd
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-08-16 18:27:18 +0530

    login interface sketching 100% over
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Login.css'
 import LoginLogo from './Images/LoginLogo.png';
+import LoginComponent from "./LoginComponent";
 
 function Login() {
     return (
         <div>
-            <p>Login Component</p>
+            <h1>Login page</h1>
             <div className="login">
                 <div>
                     <img src= {LoginLogo}  className="login-image1"/>
@@ -15,10 +16,11 @@ function Login() {
                     <h4 className="login-info2-second">User1</h4>
                 </div>
                 <div className="login-info">
-                    <div className="login-info3">
-                        <h2 className="login-info3-main">Welcome</h2>
-                        <h4 className="login-info3-second">User1</h4>
-                    </div>
+                    {/*<div className="login-info3">*/}
+                    {/*    <h2 className="login-info3-main">Login</h2>*/}
+                    {/*    <h4 className="login-info3-second">Login to get access to premium features and discounts</h4>*/}
+                    {/*</div>*/}
+                    <LoginComponent/>
                 </div>
             </div>
 
