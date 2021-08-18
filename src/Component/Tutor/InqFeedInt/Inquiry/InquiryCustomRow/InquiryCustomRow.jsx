6fb1c192d5d6363883b4720173a0096e66e95b38
AnawaratneMANA <it19086408@my.sliit.lk>
2021-08-18 17:48:31 +0530

    Inquiry Component Styles added.

--- src/Component/Tutor/InqFeedInt/Inquiry/InquiryCustomRow/InquiryCustomRow.jsx
+++ src/Component/Tutor/InqFeedInt/Inquiry/InquiryCustomRow/InquiryCustomRow.jsx
@@ -3,7 +3,15 @@ import "./InquiryCustomRow.css"
 function InquiryCustomRow({header, description}) {
     return (
         <div className="inquiry-custom-row-container">
-            <p>This is Inquiry Custom Row Component.</p>
+            <div className="inquiry-custom-row-flex">
+                <div>
+                    <div className="inquiry-custom-row-header">{header}</div>
+                    <div className="inquiry-custom-row-description">{description}</div>
+                </div>
+                <div className="custom-row-posted-time">
+                    <div className="rounded-circle"/>
+                </div>
+            </div>
         </div>
     )
 }
