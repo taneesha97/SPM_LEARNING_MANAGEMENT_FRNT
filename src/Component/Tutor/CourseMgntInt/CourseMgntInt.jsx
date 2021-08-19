5b6f4f195fdef7e1e10588b4d8595b2f4dfa73f4
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-08-19 10:37:47 +0530

    resolve errors
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Tutor/CourseMgntInt/CourseMgntInt.jsx
+++ src/Component/Tutor/CourseMgntInt/CourseMgntInt.jsx
@@ -8,41 +8,42 @@ function CourseMgntInt() {
         <React.Fragment>
             <div className="course-component">
                 <div className="input-form-container">
-                        <form className="form">
-                            <div className="course-component-header">
-                                <div className="section-header">Course Form</div>
-                                <div className="second-header">Course Form</div>
-                            </div>
+                    <form className="form">
+                        <div className="course-component-header">
+                            <div className="section-header">Course Form</div>
+                            <div className="second-header">Course Form</div>
+                        </div>
 
-                            <label htmlFor="fname">Name</label>
-                            <input type="text" id="fname" name="firstname" placeholder="Your name.." className="form-input"/>
+                        <label htmlFor="fname">Name</label>
+                        <input type="text" id="fname" name="firstname" placeholder="Your name.." className="form-input"/>
 
-                            <label htmlFor="lname">Number of Chapters</label>
-                            <input type="text" id="lname" name="lastname" placeholder="Number of Chapters.." className="form-input"/>
+                        <label htmlFor="lname">Number of Chapters</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Number of Chapters.." className="form-input"/>
 
-                            <label htmlFor="lname">Price</label>
-                            <input type="text" id="lname" name="lastname" placeholder="Price.." className="form-input"/>
+                        <label htmlFor="lname">Price</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Price.." className="form-input"/>
 
-                            <label htmlFor="lname">Description</label>
-                            <input type="text" id="lname" name="lastname" placeholder="Description.." className="form-input"/>
+                        <label htmlFor="lname">Description</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Description.." className="form-input"/>
 
-                            <div className="course-button-group button-row">
-                                <button className="dark-button">Add</button>
-                                <button className="light-button">Reset</button>
-                            </div>
-                        </form>
+                        <div className="course-button-group button-row">
+                            <button className="dark-button">Add</button>
+                            <button className="light-button">Reset</button>
+                        </div>
+                    </form>
                 </div>
                 <div className="course-table">
                     <div className="section-header dark-header"> Active Courses</div>
                     <div>
-                    <div id="style-1" className="course-table-body-scrollable">
-                        {/*Insert a custom video*/}
-                        <CustomRow/>
-                        <CustomRow/>
-                        <CustomRow/>
-                        <CustomRow/>
-                        <CustomRow/>
-                        <CustomRow/>
+                        <div id="style-1" className="course-table-body-scrollable">
+                            {/*Insert a custom video*/}
+                            <CustomRow/>
+                            <CustomRow/>
+                            <CustomRow/>
+                            <CustomRow/>
+                            <CustomRow/>
+                            <CustomRow/>
+                        </div>
                     </div>
                 </div>
             </div>
