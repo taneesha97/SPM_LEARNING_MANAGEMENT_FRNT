fd6a3c9939f7fe4010045f3bcbe5812171ad5771
AnawaratneMANA <it19086408@my.sliit.lk>
2021-08-19 17:45:19 +0530

    Data flow changed in the mini components

--- src/Pages/TutorDashboard/TutorDashboard.jsx
+++ src/Pages/TutorDashboard/TutorDashboard.jsx
@@ -6,12 +6,38 @@ import FileAttachInt from "../../Component/Tutor/FileAttachInt/FileAttachInt";
 import InqFeedInt from "../../Component/Tutor/InqFeedInt/InqFeedInt";
 
 function TutorDashboard() {
+
+    //Data Arrays.
+    //Array for the announcement component.
+    const array1 = [
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"}];
+
+    //Array for the inquiry component.
+    const array2 = [
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Nimal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"}];
+
+    //Array for the courses. course table.
+    const array3 = [
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"}];
+
     return (
         <div className="tutor-dashboard-page">
             <TutorDashHeader/>
             <CourseMgntInt/>
             <FileAttachInt/>
-            <InqFeedInt/>
+            <InqFeedInt array1={array1} array2={array2}/>
         </div>
     )
 }
