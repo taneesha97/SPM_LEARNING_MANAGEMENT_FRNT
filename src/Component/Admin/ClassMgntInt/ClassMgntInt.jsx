5a8490d7a3079b4aeaa11626305a53a93c8d8374
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2021-08-19 20:36:08 +0530

    added css to the form

--- src/Component/Admin/ClassMgntInt/ClassMgntInt.jsx
+++ src/Component/Admin/ClassMgntInt/ClassMgntInt.jsx
@@ -21,8 +21,8 @@ function ClassMgntInt(){
                               className="form-input"/>
 
                        <label htmlFor="lname">Teacher name</label>
-                       <select className="form-select" aria-label="Default select example">
-                           <option selected>Open this select menu</option>
+                       <select className="form-input" aria-label="Default select example">
+                           <option selected>Select Teacher name</option>
                            <option value="1">One</option>
                            <option value="2">Two</option>
                            <option value="3">Three</option>
