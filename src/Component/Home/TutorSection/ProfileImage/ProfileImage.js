6a0b21f6c1f44e768b23e5a0189cf8eca1a7e855
AnawaratneMANA <it19086408@my.sliit.lk>
2021-08-21 18:16:25 +0530

    TutorSection in the Homepage initialized.

---
+++ src/Component/Home/TutorSection/ProfileImage/ProfileImage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ProfileImage.css"
+function ProfileImage({image, name, designation}) {
+    return (
+        <div className="tutor-profile-container">
+            <img src="" alt=""/>
+            <div className="tutor-profile-name">Kamal Ekanayaka</div>
+            <div className="tutor-profile-designation">Software Engineering</div>
+        </div>
+    )
+}
+export default ProfileImage
