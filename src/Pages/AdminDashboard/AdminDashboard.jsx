c7c587292bfcfdafb537c6e4c78beaf2556fcc87
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2021-08-24 00:26:24 +0530

    added styles for the AnnouncementSection

--- src/Pages/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/AdminDashboard/AdminDashboard.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import "./AdminDashboard.css"
 import AdminDashHeader from "../../Component/Admin/Header/AdminDashHeader";
-
 import TeacherTableComponent from "../../Component/Admin/TeachersTable/TeacherTableComponent";
-
 import ClassMgntInt from "../../Component/Admin/ClassMgntInt/ClassMgntInt";
 import ClassDetailsTable from "../../Component/Admin/ClassTable/ClassDetailsTable";
+import AnnouncementSection from "../../Component/Admin/AnnouncementSection/AnnouncementSection";
 
 
 function AdminDashboard() {
+
+    //Array for the announcement component.
+    const array1 = [
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"},
+        {"header":"Kamal", "description":"New News Update available"}];
+
     return (
         <div className="admin-dashboard-page">
             <AdminDashHeader/>
@@ -18,6 +27,7 @@ function AdminDashboard() {
             </div>
             <br/>
             <TeacherTableComponent/>
+            <AnnouncementSection array1={array1}/>
         </div>
 
     )
