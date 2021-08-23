fd4989e9f9b9dc68cace52e484032798a2a137e4
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-08-23 15:55:58 +0530

    Popup interface over
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Profile/EmailUpdateComponent.jsx
+++ src/Component/Profile/EmailUpdateComponent.jsx
@@ -4,17 +4,17 @@ import cancel from "./images/cancel (1).png";
 import profilePic from "../../Pages/StudentProfilePage/images/profilePic.png";
 function EmailUpdateComponent(props) {
     return (props.trigger) ? (
-        <div>
+        <div className="background-userUpdate">
             <div className="emailupdatecomponent-bio">
                     <div className="emailupdatecomponent-bio1-info1">
-                        <img src= {cancel}  className="EmailCancelLogo-image1"/>
+                        <a><img src= {cancel}  className="EmailCancelLogo-image1" onClick={ () => props.setTrigger(false)}/></a>
                         <img src= {profilePic}  className="profilePic-emailUpdate"/>
 
 
                         <div className="emailupdatecomponent-bio1-info2">
-                            <h2 className="emailupdatecomponent-bio1-info2-main">User Email Update</h2>
+                            <h2 className="emailupdatecomponent-bio1-info2-main">User {props.name} Update</h2>
                             <h4 className="emailupdatecomponent-bio1-info2-second">
-                                Update Your New Email Here
+                                Update Your New {props.name} Here
                                 </h4>
                         </div>
 
--- src/Pages/StudentProfilePage/UserProfile.jsx
+++ src/Pages/StudentProfilePage/UserProfile.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './UserProfile.css'
 import profilePic from "./images/profilePic.png";
 import EmailUpdateComponent from "../../Component/Profile/EmailUpdateComponent";
 function UserProfile() {
+
+    const [buttonPopup, setButtonPopup] = useState(false);
+    const [popupName, setPopupName] = useState("");
+
+    const updateUserName = () => {
+        setButtonPopup(true);
+        setPopupName("User Name");
+    }
+
     return (<React.Fragment>
             <div>
                 <div className="bluescreen">
 
                 </div>
+
                 <div className="bluescreen1">
+
 
                 </div>
                 <div className="userprofile">
                     <div className="userprofile1">
                         <img src= {profilePic}  className="profilePic"/>
+                        <div className="popupInterfaceUpdateEmail">
+                            <EmailUpdateComponent trigger={buttonPopup} setTrigger = {setButtonPopup} name = {popupName}></EmailUpdateComponent>
+                        </div>
                         <div className="userprofile1-info">
                             <h2 className="userprofile1-info-main">Taneesha</h2>
                             <div className="profitable-group">
-                                <button className="userprofile-button1">Update User Name</button><br/>
+                                <button className="userprofile-button1" onClick={setPopupName}>Update User Name</button><br/>
                                 <button className="userprofile-button1">Update My Email</button><br/>
-                                <EmailUpdateComponent/>
                                 <button className="userprofile-button1">Update Password</button><br/>
                                 <button className="userprofile-button1">My Transactions</button><br/>
                                 <button className="userprofile-button2">Delete My Account</button><br/>
@@ -28,6 +41,7 @@ function UserProfile() {
                         </div>
                     </div>
                 </div>
+
                 <div className="userprofile-bio">
                     <div className="userprofile-bio1">
                         <div className="userprofile-bio1-info1">
