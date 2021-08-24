ec3cfe5a0a331441e46b66642f013f2c4ae7ff46
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-08-24 06:41:33 +0530

    Class Interface over
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Class/ClassPageInterfaces/ClassSearch.jsx
+++ src/Component/Class/ClassPageInterfaces/ClassSearch.jsx
@@ -6,14 +6,14 @@ import React from 'react'
 function ClassSearch() {
     return (
         <div className="row container-row row-cols-auto justify-content-center">
-                <div className="">
+                <div className="classSearch-container-1">
                     <input className="input-field-updateemail-1"
                            placeholder="Enter....."
                            type="text"
                     />
                     <button type="submit" className="profileemailupdatebtn-1"> Update </button>
                 </div>
-                <div>
+                <div className="classSearch-container-2">
                     <select
                         className="form-input-classsearch"
                         aria-label="Default select example"
@@ -24,7 +24,7 @@ function ClassSearch() {
                         <option value="3">Three</option>
                     </select>
                 </div>
-                <div>
+                <div className="classSearch-container-3">
                     <select
                         className="form-input"
                         aria-label="Default select example"
--- src/Pages/ClassPage/ClassPage.jsx
+++ src/Pages/ClassPage/ClassPage.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ClassItems from "../../Component/Class/ClassPageInterfaces/ClassItems";
 import Footer from "../../Component/Footer/Footer";
 import ClassSearch from "../../Component/Class/ClassPageInterfaces/ClassSearch";
+import './ClassPage.css'
 
 function ClassPage() {
     return (
-        <div>
+        <div className="ClassPage-Background">
             <ClassSearch/>
-            {/*<ClassItems/><br/><br/><br/>*/}
-            {/*<Footer/>*/}
+            <ClassItems/><br/><br/><br/>
+            <Footer/>
         </div>
     )
 }
