84e94d33a4cc5f4b7d660c8bcc43db2f375953c1
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-08-26 00:20:46 +0530

    new changes
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/NavigationBar/NavBarButtonComponent.jsx
+++ src/Component/NavigationBar/NavBarButtonComponent.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './NavBarButtonComponent.css'
 import redcurve from "./Images/redcurve.png";
 import greencurve from "./Images/greencurve.png";
 import {useHistory} from "react-router";
+import SucessPopUp from "../PopupModel/SucessPopUp";
 function NavBarButtonComponent() {
-
+    const [buttonPopup, setButtonPopup] = useState(false);
+    const [popupName, setPopupName] = useState("");
+    const [popupLocation, setPopupLocaion] = useState("");
     const history = useHistory();
     const profile = () => {
         history.push('/profile');
@@ -19,7 +22,9 @@ function NavBarButtonComponent() {
     }
 
     const Logout = () => {
-        history.push('/login');
+        setPopupName("logout");
+        setPopupLocaion("/login");
+        setButtonPopup(true);
 
     }
 
@@ -33,18 +38,20 @@ function NavBarButtonComponent() {
                         <h4 className="background1-info1-second">Edit your porfile here</h4>
                         <hr className="correction"/>
                     </div>
-
-                    <div className="background1-info1">
+                    <div className="login-component-1">
+                        <SucessPopUp trigger={buttonPopup} setTrigger = {setButtonPopup} name1 = {popupName} name2 = {popupLocation}></SucessPopUp>
+                    </div>
+                    <div className="background1-info1" onClick={Dashboard}>
                         <h2 className="background1-info1-main">Dashboard</h2>
                         <h4 className="background1-info1-second">Manage your classroom here</h4>
                         <hr className="correction"/>
                     </div>
-                    <div className="background1-info1">
+                    <div className="background1-info1" onClick={ClassRoom}>
                         <h2 className="background1-info1-main">My ClassRooms</h2>
                         <h4 className="background1-info1-second">See all My ClassRooms</h4>
                         <hr className="correction"/>
                     </div>
-                    <div className="background1-info1">
+                    <div className="background1-info1" onClick={Logout}>
                         <h2 className="background1-info1-main">Logout</h2>
                         <h4 className="background1-info1-second">See you again...!!</h4>
                     </div>
