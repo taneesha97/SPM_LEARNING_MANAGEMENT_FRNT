14e9ae72f153e5469c9877197806289dbeeeb640
AnawaratneMANA <it19086408@my.sliit.lk>
2021-08-25 15:56:33 +0530

    Added shadows to the course table.

--- src/Component/Tutor/FileAttachInt/FileAttachmentTable/FileAttachmentTable.jsx
+++ src/Component/Tutor/FileAttachInt/FileAttachmentTable/FileAttachmentTable.jsx
@@ -1,38 +1,39 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import { red } from "@material-ui/core/colors";
 
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+    { id: "name", label: "Name", minWidth: 170 },
+    { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
     {
-        id: 'population',
-        label: 'Population',
+        id: "population",
+        label: "Population",
         minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
+        align: "right",
+        format: (value) => value.toLocaleString("en-US")
     },
     {
-        id: 'size',
-        label: 'Size\u00a0(km\u00b2)',
+        id: "size",
+        label: "Size\u00a0(km\u00b2)",
         minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
+        align: "right",
+        format: (value) => value.toLocaleString("en-US")
     },
     {
-        id: 'density',
-        label: 'Density',
+        id: "density",
+        label: "Density",
         minWidth: 170,
-        align: 'right',
-        format: (value) => value.toFixed(2),
-    },
+        align: "right",
+        format: (value) => value.toFixed(2)
+    }
 ];
 
 function createData(name, code, population, size) {
@@ -41,37 +42,66 @@ function createData(name, code, population, size) {
 }
 
 const rows = [
-    createData('India', 'IN', 1324171354, 301340),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
+    createData("India", "IN", 1324171354, 301340),
+    createData("China", "CN", 1403500365, 9596961),
+    createData("Italy", "IT", 60483973, 301340),
+    createData("United States", "US", 327167434, 9833520),
+    createData("Canada", "CA", 37602103, 9984670),
+    createData("Australia", "AU", 25475400, 7692024),
+    createData("Germany", "DE", 83019200, 357578),
+    createData("Ireland", "IE", 4857000, 70273),
+    createData("Mexico", "MX", 126577691, 1972550),
+    createData("Japan", "JP", 126317000, 377973),
+    createData("France", "FR", 67022000, 640679),
+    createData("United Kingdom", "GB", 67545757, 242495),
+    createData("Russia", "RU", 146793744, 17098246),
+    createData("Nigeria", "NG", 200962417, 923768),
+    createData("Brazil", "BR", 210147125, 8515767)
 ];
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        "&:nth-of-type(odd)": {
+            backgroundColor: theme.palette.action.hover
+        }
+    }
+}))(TableRow);
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: "#7364B9",
+        color: theme.palette.common.white,
+        fontWeight: 600
+    },
+    body: {
+        fontSize: 15
+    }
+}))(TableCell);
 
 const useStyles = makeStyles({
     root: {
-        width: '100%',
+        width: "100%",
+        borderRadius: 30,
+        display: "block",
+        overflow: "hidden",
+        backgroundColor: red
     },
     container: {
-        maxHeight: 410,
         borderRadius: 30,
-    },
+        overflow: "scroll",
+        scrollbarWidth: "none" /* Firefox */,
+        maxHeight: 420,
+        "&::-webkit-scrollbar": {
+            display: "none"
+        } /* Chrome */
+    }
+
 });
 
 export default function FileAttachmentTable() {
     const classes = useStyles();
     const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [rowsPerPage, setRowsPerPage] = React.useState(7);
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -85,35 +115,44 @@ export default function FileAttachmentTable() {
     return (
         <Paper className={classes.root}>
             <TableContainer className={classes.container}>
-                <Table stickyHeader aria-label="sticky table">
+                <Table stickyHeader aria-label="customized table">
                     <TableHead>
-                        <TableRow>
+                        <StyledTableRow>
                             {columns.map((column) => (
-                                <TableCell
+                                <StyledTableCell
                                     key={column.id}
                                     align={column.align}
                                     style={{ minWidth: column.minWidth }}
                                 >
                                     {column.label}
-                                </TableCell>
+                                </StyledTableCell>
                             ))}
-                        </TableRow>
+                        </StyledTableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                            return (
-                                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                    {columns.map((column) => {
-                                        const value = row[column.id];
-                                        return (
-                                            <TableCell key={column.id} align={column.align}>
-                                                {column.format && typeof value === 'number' ? column.format(value) : value}
-                                            </TableCell>
-                                        );
-                                    })}
-                                </TableRow>
-                            );
-                        })}
+                        {rows
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row) => {
+                                return (
+                                    <StyledTableRow
+                                        hover
+                                        role="checkbox"
+                                        tabIndex={-1}
+                                        key={row.code}
+                                    >
+                                        {columns.map((column) => {
+                                            const value = row[column.id];
+                                            return (
+                                                <TableCell key={column.id} align={column.align}>
+                                                    {column.format && typeof value === "number"
+                                                        ? column.format(value)
+                                                        : value}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </StyledTableRow>
+                                );
+                            })}
                     </TableBody>
                 </Table>
             </TableContainer>
