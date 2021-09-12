d05d35b8bf4ae3cb36719720c2d678e49dc05e8f
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-12 19:18:31 +0530

    User profile responsiveness work out part 1 - over
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Pages/ProfilePage/UserProfile.jsx
+++ src/Pages/ProfilePage/UserProfile.jsx
@@ -26,6 +26,18 @@ function UserProfile() {
                 </div>
 
                 <div className="bluescreen1">
+                    <div className="userprofile-bio">
+                        <div className="userprofile-bio1">
+                            <div className="userprofile-bio1-info1">
+                                <h2 className="userprofile-bio1-info1-main">Bio</h2><br/>
+                                <h4 className="userprofile-bio1-info1-second">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Curabitur nec dignissim sem. Donec sed justo rutrum, vehicula elit a, pulvinar sem. Proin convallis,
+                                    orci vel blandit luctus, massa lectus blandit neque,
+                                    sit amet pharetra libero lacus et arcu. Donec placerat lacinia nunc vel faucibus.</h4>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
                 <div className="userprofile">
                     <div className="userprofile1">
@@ -42,23 +54,11 @@ function UserProfile() {
                                 <button className="userprofile-button1">My Transactions</button><br/>
                                 <button className="userprofile-button2">Delete My Account</button><br/>
                             </div>
-
                         </div>
                     </div>
                 </div>
 
-                <div className="userprofile-bio">
-                    <div className="userprofile-bio1">
-                        <div className="userprofile-bio1-info1">
-                            <h2 className="userprofile-bio1-info1-main">Bio</h2><br/>
-                            <h4 className="userprofile-bio1-info1-second">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Curabitur nec dignissim sem. Donec sed justo rutrum, vehicula elit a, pulvinar sem. Proin convallis,
-                                orci vel blandit luctus, massa lectus blandit neque,
-                                sit amet pharetra libero lacus et arcu. Donec placerat lacinia nunc vel faucibus.</h4>
-                        </div>
 
-                    </div>
-                </div>
 
             </div>
     </React.Fragment>
