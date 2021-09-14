fa8f3d1bb0af234796573304b06901f4e1134d2c
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-14 20:01:19 +0530

    popup interface updated
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Profile/EmailUpdateComponent.jsx
+++ src/Component/Profile/EmailUpdateComponent.jsx
@@ -4,46 +4,45 @@ import cancel from "./images/cancel (1).png";
 import profilePic from "../../Pages/ProfilePage/images/profilePic.png";
 function EmailUpdateComponent(props) {
 
-    const crypto = require('crypto'),
-        hash = crypto.getHashes();
-    let password = crypto.createHash('sha1').update('value').digest('hex');
-    console.log(password);
-    async function compareIt(password){
-       // const validPassword = await crypto.;
-        return password;
-    }
-    console.log(compareIt("value"));
+    //needed to added to login
+
+    // const crypto = require('crypto'),
+    //     hash = crypto.getHashes();
+    // let password = crypto.createHash('sha1').update('value').digest('hex');
+    // console.log(password);
+    // let password2 = crypto.createHash('sha1').update('value').digest('hex');
+    // console.log(password2);
+    // if (password === password2){
+    //     console.log('1');
+    // }else{
+    //     console.log('2');
+    // }
+
+
+
     return (props.trigger) ? (
         <div className="background-userUpdate-1">
-            <div className="background-userUpdate">
                 <div className="emailupdatecomponent-bio">
                     <div className="emailupdatecomponent-bio1-info1">
                         <a><img src= {cancel}  className="EmailCancelLogo-image1" onClick={ () => props.setTrigger(false)}/></a>
                         <img src= {profilePic}  className="profilePic-emailUpdate"/>
-
                         <div className="emailupdatecomponent-bio1-info2">
-                            <h2 className="emailupdatecomponent-bio1-info2-main">User's new {props.name} Update</h2>
-                            <h4 className="emailupdatecomponent-bio1-info2-second">
-                                Update Your New {props.name} Here
-                            </h4>
+                                <h2 className="emailupdatecomponent-bio1-info2-main text-white">{props.name} Update</h2>
+                                <div className="emailupdatecomponent-bio1-info2-main1 row mb-3 mt-2">
+                                    <div className="col">
+                                        <h6 className="emailupdatecomponent-bio1-info2-second">Current Username: Salitha1</h6>
+                                    </div>
+                                </div>
                         </div>
-
-
-
                         <div className="emailupdatecomponent-bio1-info1-second">
                             <form>
                                 <div className="form-group mt-2">
-                                    <label htmlFor="exampleInputEmail1">Email address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1"
-                                           aria-describedby="emailHelp" placeholder="Enter email"/>
-                                </div>
-                                <div className="form-group mt-2">
-                                    <label htmlFor="exampleInputPassword1">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1"
-                                           placeholder="Password"/>
+                                    <label htmlFor="exampleInputUsername">Username</label>
+                                    <input type="text" className="form-control" id="exampleInputUsername"
+                                           aria-describedby="emailHelp" placeholder="Enter Username"/>
                                 </div>
                                 <div className="button-container mt-2">
-                                    <button type="submit" className="btn btn-primary">Submit</button>
+                                    <button type="submit" className="btnpopup btn btn-primary mt-2">Submit</button>
                                 </div>
                             </form>
 
@@ -51,7 +50,6 @@ function EmailUpdateComponent(props) {
                     </div>
 
                 </div>
-            </div>
         </div>
 
     ) : "";
