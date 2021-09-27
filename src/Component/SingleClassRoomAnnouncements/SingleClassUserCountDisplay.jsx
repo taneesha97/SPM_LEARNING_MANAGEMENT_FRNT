818e71149068847384006e25d58e9a50a1219cfc
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-27 20:36:37 +0530

    single class view update 8 count interface update 2 usercount interface added
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/SingleClassRoomAnnouncements/SingleClassUserCountDisplay.jsx
+++ src/Component/SingleClassRoomAnnouncements/SingleClassUserCountDisplay.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
 import './style.css'
+import SingleClassUserCountDisplayInterface1 from "./SingleClassUserCountDisplayInterface1";
 
 function SingleClassUserCountDisplay() {
     return (
         <div className="singleclassusercountdisplay">
-            <h1>dddddddddddd</h1>
-            {/*<div className="userprofile1">*/}
-            {/*    <img src= {profilePic}  className="profilePic"/>*/}
-            {/*    <div className="popupInterfaceUpdateEmail">*/}
-            {/*        <UserDetailsUpdateComponent trigger={buttonPopup} setTrigger = {setButtonPopup} name = {result} name1 = {popupName} buttonPopup2 = {buttonPopup2} setButtonPopup2 = {setButtonPopup2}></UserDetailsUpdateComponent>*/}
-            {/*        <PasswordUpdateComponent trigger={buttonPopup1} setTrigger = {setButtonPopup1} name = {popupName}></PasswordUpdateComponent>*/}
-            {/*    </div>*/}
-            {/*    <div className="userprofile1-info">*/}
-            {/*        <h2 className="userprofile1-info-main">Taneesha</h2>*/}
-            {/*        <div className="profitable-group">*/}
-            {/*            <button className="userprofile-button1" onClick={updateEmail}>Update My Personal Details</button><br/>*/}
-            {/*            <button className="userprofile-button1" onClick={updatePassword}>Update Password</button><br/>*/}
-            {/*            <button className="userprofile-button1">My Transactions</button><br/>*/}
-            {/*            <button  className="userprofile-button2" onClick={handleShow}>Delete My Account</button><br/>*/}
-
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <SingleClassUserCountDisplayInterface1/><br/>
+            <SingleClassUserCountDisplayInterface1/>
         </div>
     )
 }
---
+++ src/Component/SingleClassRoomAnnouncements/SingleClassUserCountDisplayInterface1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+function SingleClassUserCountDisplayInterface1() {
+    return (
+        <div className="SingleClassUsertDisplayInterface1">
+            <h1 className="announcementDisplay1-font1">qqq</h1>
+            <h2 className="announcementDisplay1-font2">www</h2>
+        </div>
+    )
+}
+
+export default SingleClassUserCountDisplayInterface1
