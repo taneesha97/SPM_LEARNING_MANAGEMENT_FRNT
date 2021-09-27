c7434f6bb4e625cdf5735e231de2b0a84d30e528
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-27 16:40:07 +0530

    Registration back footer added
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Pages/Registration/RegistrationPage.jsx
+++ src/Pages/Registration/RegistrationPage.jsx
@@ -4,9 +4,9 @@ import './style.css'
 
 function RegistrationPage() {
     return (
-        <div style={{backgroundColor:'#D6D6D6'}} className="background-reg">
-            <Registration/><br/><br/>
-            <div className='row sub-footer2'>Lerning Managemnt System v1.0 || All Rights Received.</div>
+        <div  className="background-reg">
+            <Registration/>
+
         </div>
     )
 }
