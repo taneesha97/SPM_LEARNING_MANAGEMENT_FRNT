1b3ec53d86f1988193f485b2d3c2b47429c8fb02
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-28 17:02:14 +0530

    single class view update 16 count interface update 10 completeness 90%
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/SingleClassRoomAnnouncements/SingleClassAnnouncements.jsx
+++ src/Component/SingleClassRoomAnnouncements/SingleClassAnnouncements.jsx
@@ -13,12 +13,7 @@ function SingleClassAnnouncements() {
         {"title1": 'Announcement',"title2": 'Consider Two Announcements – One announcement to let students know that you are actively\n' +
                 'working on making adjustments to the course, and the second to let them know what the\n' +
                 'course modifications are, once you have them completed. '},
-        {"title1": 'Announcement',"title2": 'Consider Two Announcements – One announcement to let students know that you are actively\n' +
-                'working on making adjustments to the course, and the second to let them know what the\n' +
-                'course modifications are, once you have them completed. '},
-        {"title1": 'Announcement',"title2": 'Consider Two Announcements – One announcement to let students know that you are actively\n' +
-                'working on making adjustments to the course, and the second to let them know what the\n' +
-                'course modifications are, once you have them completed. '},
+
       ]
     return (
             <div className="singleclassviewbackgroundann">
