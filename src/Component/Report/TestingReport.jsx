dc0cb2fcc2fdb5335da1af936c3912294e5ffe9e
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-29 19:26:38 +0530

    update 1
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Report/TestingReport.jsx
+++ src/Component/Report/TestingReport.jsx
@@ -1,4 +1,4 @@
-import React, {useState, Component } from 'react'
+import React, {useState, Component, useEffect} from 'react'
 import { jsPDF } from "jspdf";
 import { FiDownload } from "react-icons/fi"
 import {Chart} from "chart.js";
@@ -11,6 +11,8 @@ import html2canvas from "html2canvas";
 const pdfConverter = require("jspdf");
 
 function TestingReport() {
+    const [chartData, setChartData] = useState({})
+
     const cData = {
         labels: ["L 1", "L 2", "L 3", "L 4", "L 5"],
         datasets: [
@@ -22,14 +24,36 @@ function TestingReport() {
         ]
     };
 
+    useEffect(() => {
+        setChartData({
+            // labels: data.data.map((crypto) => crypto.name),
+            datasets: [
+                {
+                    label: "Price in USD",
+                    data: cData,
+                    backgroundColor: [
+                        "#ffbb11",
+                        "#ecf0f1",
+                        "#50AF95",
+                        "#f3ba2f",
+                        "#2a71d0"
+                    ]
+                }
+            ]
+        });
+    },[]);
+
+
+
 
 
     const getData = () => {
+       // chartRef = React.createRef();
         const canvas = document?.querySelector('canvas'),
-            ctx = canvas.getContext('2d');
-        //new Chart(ctx).Line(cData);
+           ctx = canvas.getContext('2d');
+        // new Chart(ctx).Line(cData);
         // new Chart(ctx,  {
-        //     type: 'bar',
+        //     type: 'line',
         //     data: cData,
         //     options: {
         //         scales: {
@@ -50,8 +74,8 @@ function TestingReport() {
         //     height: 200
         //
         // });
-        const img = canvas.toDataURL("image/png");
-            const pdf = new jsPDF('landscape');
+       // const img = canvas.toDataURL("image/png");
+            const pdf = new jsPDF();
             //doc.addImage(Logo, "png", 10, 10, 30, 5);
             pdf.setFontSize(20);
             pdf.text("Summary Report", 75, 15);
@@ -70,6 +94,18 @@ function TestingReport() {
                 "png",
                 10, 70, 280, 150
             );
+            //pdf.table(3,4,)
+//         pdf.autoTable({ html: '#my-table' })
+//
+// // Or use javascript directly:
+//         doc.autoTable({
+//             head: [['Name', 'Email', 'Country']],
+//             body: [
+//                 ['David', 'david@example.com', 'Sweden'],
+//                 ['Castille', 'castille@example.com', 'Spain'],
+//                 // ...
+//             ],
+//         })
             pdf.save("chart.pdf");
     };
 
@@ -80,23 +116,23 @@ function TestingReport() {
             <h1>eeeee</h1>
 
             <div>
-                <div className="div2PDF">
-                    <Bar
-                       // data={cData}
-                        // options={{
-                        //     title: {
-                        //         display: true,
-                        //         text: "Chart to PDF Demo",
-                        //         fontSize: 32
-                        //     },
-                        //     legend: {
-                        //         display: true,
-                        //         position: "right"
-                        //     }
-                        // }}
-                        height={200}
-                    />
-                </div>
+                <Chart chartData={chartData} />
+
+                    {/*<Bar*/}
+                    {/*   data={cData}*/}
+                    {/*    options={{*/}
+                    {/*        title: {*/}
+                    {/*            display: true,*/}
+                    {/*            text: "Chart to PDF Demo",*/}
+                    {/*            fontSize: 32*/}
+                    {/*        },*/}
+                    {/*        legend: {*/}
+                    {/*            display: true,*/}
+                    {/*            position: "right"*/}
+                    {/*        }*/}
+                    {/*    }}*/}
+                    {/*    height={200}*/}
+                    {/*/>*/}
                 <div>
                     <button onClick={getData}>Export 2 PDF</button>
                 </div>
