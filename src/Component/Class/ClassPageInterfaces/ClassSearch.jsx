81db9ede2b1ebb3e660cf44b035f071e43a90a8d
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2021-09-30 01:01:06 +0530

    search bar state added

--- src/Component/Class/ClassPageInterfaces/ClassSearch.jsx
+++ src/Component/Class/ClassPageInterfaces/ClassSearch.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ClassSearch.css';
 
-function ClassSearch() {
+function ClassSearch({searchData}) {
+
+    const [searchTerm, setSearchTerm] = useState("");
     return (
         <div className="search_back">
         <div class="container">
             <div className="search_wrap search_wrap_5">
                     <div className="search_box">
-                        <input type="text" className="input" placeholder="search..."/>
+                        <input type="text"
+                               className="input"
+                               placeholder="search..."
+                               value={searchTerm}
+                               onChange={(event) => setSearchTerm(event.target.value)}
+
+                        />
                         <div className="btn">
                             <p className="btn_font">Search</p>
                         </div>
--- src/Pages/ClassPage/ClassPage.jsx
+++ src/Pages/ClassPage/ClassPage.jsx
@@ -3,12 +3,14 @@ import ClassItems from "../../Component/Class/ClassPageInterfaces/ClassItems";
 import Footer from "../../Component/Footer/Footer";
 import ClassSearch from "../../Component/Class/ClassPageInterfaces/ClassSearch";
 import './ClassPage.css'
+import {useSelector} from "react-redux";
 
 function ClassPage() {
+    const searchData = useSelector((state) => state.classes.classRecords.records);
     return (
      
         <div style={{backgroundColor:'#D6D6D6'}}>
-            <ClassSearch/>
+            <ClassSearch searchData={searchData}/>
             <ClassItems/><br/><br/><br/>
             <Footer/>
         </div>
