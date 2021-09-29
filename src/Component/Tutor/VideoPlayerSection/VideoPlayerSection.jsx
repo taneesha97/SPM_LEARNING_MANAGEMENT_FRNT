276b6281ed771234288be313df213c0b84ba1f3c
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-29 15:16:08 +0530

    Video Section API call and method created. (Fetching Video URLs)

--- src/Component/Tutor/VideoPlayerSection/VideoPlayerSection.jsx
+++ src/Component/Tutor/VideoPlayerSection/VideoPlayerSection.jsx
@@ -1,12 +1,34 @@
 import './VideoPlayerSection.scss'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Container} from "@material-ui/core";
 import PlayerThumbnail from "../../../Pages/VideoResourcePage/PlayerThumbnail/PlayerThumbnail";
 import CustomRow from "../CourseMgntInt/CustomRow/CustomRow";
 import Grid from "@material-ui/core/Grid";
+import axios from "axios";
 
 
 function VideoPlayerSection() {
+
+    /**
+     * This method should be replaced by a API call.
+     * **/
+
+    const [videoData, setVideoData] = useState([]);
+
+    useEffect(() => {
+       getVieoUrls();
+    },[])
+
+    async function getVieoUrls () {
+        const response = await axios.get("http://localhost:8073/videos");
+        const data = response.data;
+        const options = data.map(item => ({
+            "image" : item.name,
+            "url" : item.uri,
+        }))
+        setVideoData(options);
+    }
+
 
     const array1 =[
         {"image":"http://placeimg.com/180/100/nature", "url":"https://www.youtube.com/watch?v=wGixQPuG1GY"},
@@ -22,7 +44,7 @@ function VideoPlayerSection() {
             <Container maxWidth="md">
                 <p className="header-video-player">Course Videos</p>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 3, md: 4 }}>
-                {array1?.map((item) => (
+                {videoData?.map((item) => (
                     <Grid item xs={7} md={3} xl={3} sm={4}>
                         <PlayerThumbnail image={item.image} url={item.url}/>
                     </Grid>
