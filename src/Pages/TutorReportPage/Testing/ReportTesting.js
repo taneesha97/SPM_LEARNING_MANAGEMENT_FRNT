74d632bbb568a939fb6e8b96e25dbc1bf786459a
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-30 21:13:49 +0530

    Errors Fixed. with CSS.

--- src/Pages/TutorReportPage/Testing/ReportTesting.js
+++ src/Pages/TutorReportPage/Testing/ReportTesting.js
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react'
-import './Report.css';
+// import './Report.css';
 import '@progress/kendo-theme-material/dist/all.css';
 import { Button } from '@progress/kendo-react-buttons';
 import {PDFExport, savePDF} from "@progress/kendo-react-pdf";
