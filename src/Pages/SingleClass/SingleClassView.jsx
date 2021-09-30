e9a49f1bd8cbed603657ae9fbc3514f20b23ab0e
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-30 14:07:00 +0530

    Single class view get call done
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Pages/SingleClass/SingleClassView.jsx
+++ src/Pages/SingleClass/SingleClassView.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SingleClassWelcomeHeader from "../../Component/WelcomeHeader-Course/SingleClassWelcomeHeader";
 import SingleClassViewBackground from "./SingleClassViewBackground";
 import CourseContent from "../../Component/CourseContent/CourseContent";
@@ -6,13 +6,32 @@ import Footer from "../../Component/Footer/Footer";
 import './style.css'
 import SingleClassAnnouncements from "../../Component/SingleClassRoomAnnouncements/SingleClassAnnouncements";
 import SingleClassUserCountDisplay from "../../Component/SingleClassRoomAnnouncements/SingleClassUserCountDisplay";
+import {useDispatch, useSelector} from "react-redux";
+import {getAnnouncements} from "../../Action/Announcement";
 function SingleClassView() {
+
+    const dispatch = useDispatch();
+    const [announcementData, setAnnouncementData] = useState('');
+
+
+    const annDetails = useSelector((state) => state.Announcement?.announcementRecords?.records);
+    console.log('announcements ', annDetails)
+
+
+
+    React.useEffect(() => {
+        console.log('Calling ann');
+        dispatch(getAnnouncements());
+        setAnnouncementData(annDetails)
+    }, []);
+
+    console.log(announcementData)
     return (
         <div className="singleclassViewBackground">
             <SingleClassWelcomeHeader/>
             <SingleClassUserCountDisplay/>
             <CourseContent/>
-            <SingleClassAnnouncements/>
+            <SingleClassAnnouncements annoucemntData = {annDetails} setAnnoucementData = {setAnnouncementData}/>
 
 
             {/*<SingleClassViewBackground/>*/}
