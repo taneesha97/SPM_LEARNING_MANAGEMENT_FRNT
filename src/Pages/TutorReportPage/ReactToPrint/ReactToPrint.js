52fdbb2ecc1301b8a0560b67abc59df4e7fab231
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-30 16:47:12 +0530

    Applied some styles to the printing component.

--- src/Pages/TutorReportPage/ReactToPrint/ReactToPrint.js
+++ src/Pages/TutorReportPage/ReactToPrint/ReactToPrint.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useRef} from 'react'
 import ReactToPrint from "react-to-print";
+import "./ReactToPrint.scss";
 import ReportTesting from "../Testing/ReportTesting";
 import PrintableComponent from "./PrintableComponent/PrintableComponent";
+import Button from "@progress/kendo-react-buttons/dist/es/Button";
 /**
  * If work make this  class a reusable class passing the component as props (Default component)
  * Component to print shoudl be passed inside to this class <PrintableComponent/>
@@ -14,16 +16,25 @@ function ReactToPrintClass() {
 
     useEffect(() => {
 
-    },[])
+    }, [])
 
     return (
-        <div style={{backgroundColor:'#949494', borderRadius:"10px"}}>
-            <PrintableComponent ref={el => (componentRef = el)} />
+        <div className="parent-container">
             <ReactToPrint
-                trigger={() => <a href="#">Print this out!</a>}
+                trigger={() =>
+
+                    <div className="printing-component-container">
+                        <div className="printing-component-header">PDF Report Generator</div>
+                        <div className="printing-component-sub-header">PDF Report Generator</div>
+                        <a className="printing-component-a" href="#">
+                            <Button style={{backgroundColor: "red", color: "white", marginTop: "40px"}}>Print this
+                                out!</Button>
+                        </a>
+                    </div>
+                }
                 content={() => componentRef}
             />
-
+            <PrintableComponent className="component-to-print" ref={el => (componentRef = el)}/>
         </div>
     )
 }
