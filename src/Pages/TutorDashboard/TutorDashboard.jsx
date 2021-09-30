471882b429321788a2f3f4d764fd6746eb2c516a
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-30 12:12:12 +0530

    Authentication added to admindash and tutor dash
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Pages/TutorDashboard/TutorDashboard.jsx
+++ src/Pages/TutorDashboard/TutorDashboard.jsx
@@ -27,12 +27,12 @@ function TutorDashboard() {
 
     //User Validation Upon Landing On the Page -- Temporarily Disable.
     useEffect(()=> {
-        // if(usertype != "teacher" && usertype != "admin"){
-        //     history.push('./login')
-        //     //window.location.href='/login';
-        // } else {
-        //     //setFlag(true);
-        // }
+        if(usertype != "teacher" && usertype != "admin"){
+            history.push('./login')
+            //window.location.href='/login';
+        } else {
+            //setFlag(true);
+        }
         getItems();
     }, [])
 
