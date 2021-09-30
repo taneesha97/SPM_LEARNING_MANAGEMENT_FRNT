7a6b68a0e93d54b0c0a0b5b00e7082b4a1f90431
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-09-30 10:35:20 +0530

    class search over
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Class/ClassPageInterfaces/ClassSearch.jsx
+++ src/Component/Class/ClassPageInterfaces/ClassSearch.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './ClassSearch.scss';
 
-function ClassSearch() {
+function ClassSearch(props) {
     return (
         <div className="search_back">
         <div class="container">
             <div className="search_wrap search_wrap_5">
                     <div className="search_box">
-                        <input type="text" className="input" placeholder="search..."/>
+                        <input type="text"
+                               className="input"
+                               placeholder="search..."
+                               value={props?.searchData}
+                               onChange={(event) => props?.setSearchTerm(event.target.value)}
+
+                        />
                         <div className="btn">
                             <p className="btn_font">Search</p>
                         </div>
