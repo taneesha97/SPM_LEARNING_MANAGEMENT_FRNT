145f47f22667b8b4c3cb540584bb72bbd2c41a71
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2021-10-02 20:18:45 +0530

    class tile modify

--- src/Component/Class/ClassTile/ClassTile.jsx
+++ src/Component/Class/ClassTile/ClassTile.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import "../../../../node_modules/bootstrap/dist/css/bootstrap.css"
 import './ClassFileStyles.css'
 import {useHistory} from "react-router";
-import {Card, CardMedia} from "@material-ui/core";
+import {Card, CardMedia, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        // ⚠️ object-fit is not supported by IE11.
+        objectFit: 'cover',
+    },
+});
+
 const ClassTile = (props) => {
     console.log('ROWS',props?.rows?.fullPath);
     const history = useHistory();
-
+    // const { classes } = props;
+    const classes = useStyles();
     const passData = () => {
         history.push('/singleclassview', { data: props?.rows })
     }
@@ -18,12 +30,14 @@ const ClassTile = (props) => {
                 <div className='sub-heading'> {props?.rows?.description}</div>
             </div>
             <div className='right-container'>
-                <Card>
-                    <CardMedia image="D:\sliit\3rd Year\2nd sem\SPM\Assignment\SPM_LEARNING_MANAGEMENT_BACK_New\img\img_2.jpg">
-
-                    </CardMedia>
+                <Card >
+                    {/*<CardMedia*/}
+                    {/*    component="img"*/}
+                    {/*    height="140"*/}
+                    {/*    className={classes.media}*/}
+                    {/*    image={require('D:/sliit/3rd Year/2nd sem/SPM/Assignment/SPM_LEARNING_MANAGEMENT_BACK_New/img/img_2.jpg')}>*/}
+                    {/*</CardMedia>*/}
                 </Card>
-                {/*<img />*/}
             </div>
 
         </div>
