9cb5a3df1b7dfb2d79a56be75961280e8bc9793a
Salitha Ekanayaka <68500118+salithaekanayaka123@users.noreply.github.com>
2021-10-02 20:15:17 +0530

    Imqage show done and dusted
    Signed-off-by: Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>

--- src/Component/Class/ClassTile/ClassTile.jsx
+++ src/Component/Class/ClassTile/ClassTile.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import "../../../../node_modules/bootstrap/dist/css/bootstrap.css"
 import './ClassFileStyles.css'
 import {useHistory} from "react-router";
-import {Card, CardMedia} from "@material-ui/core";
+import {Card, CardMedia, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    valuew : {
+        height: "200px"
+    }
+});
 const ClassTile = (props) => {
-    console.log('ROWS',props?.rows?.fullPath);
+
+    console.log('ROWS',props?.rows?.image);
     const history = useHistory();
+    const classes = useStyles();
 
     const passData = () => {
         history.push('/singleclassview', { data: props?.rows })
     }
+
+    //const onMediaFallback = event => event.target.src = FALLBACK_IMAGE;
     // console.log(props.row.fullPath);
     return(
         <div className='class-tile-container' onClick={passData}>
@@ -19,7 +29,14 @@ const ClassTile = (props) => {
             </div>
             <div className='right-container'>
                 <Card>
-                    <CardMedia image="D:\sliit\3rd Year\2nd sem\SPM\Assignment\SPM_LEARNING_MANAGEMENT_BACK_New\img\img_2.jpg">
+                    <CardMedia
+                        component="img"
+                        //C:\sliit\3rd Year\2nd Semester\SPM\New folder\download.jpg
+                        //`https://localhost:8073/...../${props}`
+                        style={{height: 300, width: 200, paddingTop: '56.25%', marginTop:'30'}}
+                        image ={`http://localhost:8073/api/download/image/${props?.rows?.image}`}
+                     >
+
 
                     </CardMedia>
                 </Card>
